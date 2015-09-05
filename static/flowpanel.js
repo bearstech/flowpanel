9cde9542581fad960e57275e404eb03eb9768db3
Mathieu Lecarme <mlecarme@bearstech.com>
2015-09-06 01:09:02 +0200

    HTML UI POC.

--- static/flowpanel.js
+++ static/flowpanel.js
@@ -1,16 +1,22 @@
-var connection = new WebSocket('ws://localhost:8080/chaussette');
+$(function() {
 
-// When the connection is open, send some data to the server
-connection.onopen = function () {
-    connection.send('Ping'); // Send the message 'Ping' to the server
-};
+    var connection = new WebSocket('ws://localhost:8080/chaussette');
 
-// Log errors
-connection.onerror = function (error) {
-    console.log('WebSocket Error ' + error);
-};
+    // When the connection is open, send some data to the server
+    connection.onopen = function () {
+        connection.send('Ping'); // Send the message 'Ping' to the server
+    };
 
-// Log messages from the server
-connection.onmessage = function (e) {
-    console.log('Server: ' + e.data);
-};
+    // Log errors
+    connection.onerror = function (error) {
+        console.log('WebSocket Error ' + error);
+    };
+
+    // Log messages from the server
+    connection.onmessage = function (e) {
+        console.log('Server: ' + e.data);
+        $('#events').append('<ul>' + e.data);
+    };
+
+
+});
