9b55795cff4482df761dc04452268cad98e6f833
Mathieu Lecarme <mlecarme@bearstech.com>
2015-09-06 00:07:59 +0200

    Better API usage.

--- flowpanel/auth.py
+++ flowpanel/auth.py
@@ -1,5 +1,5 @@
 import asyncio
-import base64
+from base64 import b64decode
 
 from aiohttp.web import HTTPUnauthorized
 
@@ -12,13 +12,13 @@ def auth_factory(app, handler):
     @asyncio.coroutine
     def middleware(request):
         if 'AUTHORIZATION' not in request.headers:
-            return web.Response(
-                headers={'WWW-Authenticate': 'Basic realm="flowpanel"'},
-                status=HTTPUnauthorized.status_code)
+            raise HTTPUnauthorized(
+                    headers={'WWW-Authenticate': 'Basic realm="flowpanel"'}
+            )
         else:
             auth, loginpassword = request.headers['AUTHORIZATION'].split(" ", 2)
             assert auth == "Basic"
-            login, password = base64.b64decode(loginpassword).decode('utf-8').split(':', 2)
+            login, password = b64decode(loginpassword).decode('utf-8').split(':', 2)
             # FIXME, validate the password
             request[USER_KEY] = login
 
